70e7e0f0a2ebeabeba3ee2b9233b520e4363d880
Yibin Long <yibinlong@outlook.com>
2020-02-20 13:33:14 -0500

    parse thru 1st and 2nd line

--- src/BookScanning.java
+++ src/BookScanning.java
@@ -3,18 +3,29 @@ import org.omg.CORBA.INTERNAL;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BookScanning {
     public static void main(String[] args) {
         Scanner result = readLine();
-        System.out.println(result);
 
         String firstLine = result.nextLine();
         String[] firstLineSplit = firstLine.split(" ");
         int bookNum = Integer.parseInt(firstLineSplit[0]);
         int libNum = Integer.parseInt(firstLineSplit[1]);
         int dayNum = Integer.parseInt(firstLineSplit[2]);
+
+        String bookScoreResult = result.nextLine();
+        String[] bookScoresSplit = bookScoreResult.split(" ");
+        int[] bookScore = new int[bookScoresSplit.length];
+        int count = 0;
+        for (String s: bookScoresSplit){
+            bookScore[count] = Integer.parseInt(s);
+            count++;
+        }
+        System.out.println(Arrays.toString(bookScore));
+
 
         int lineNum = 0;
         while(result.hasNextLine()){
